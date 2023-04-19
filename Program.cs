d7d4c741de633f76a433b407e47be78f4f100a16
4elovel <marcynakvova007@gmail.com>
2023-04-19 17:36:03 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FilesRW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "file.txt";
+            Console.WriteLine("Write text that you want to append:");
+            string text = Console.ReadLine();
+            using (FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write, FileShare.None))
+            {
+                using(StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(text);
+                }
+            }
+        }
+    }
+}
